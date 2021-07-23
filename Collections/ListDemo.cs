e847d5b048cf3106e179e31853219b226d8b5945
unknown <sivaranjanib99@gmail.com>
2021-07-23 22:59:27 +0530

    [siva] adding the string to the list

---
+++ Collections/ListDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class ListDemo
+    {
+        //method to display the list functions
+        public static void List()
+        {
+            Console.WriteLine("Creating list");
+            //creating new list
+            List<string> list = new List<string>();
+            //adding object to the list
+            list.Add("siva");
+            list.Add("hello");
+            list.Add("hi");
+            list.Add("welcome");
+            Console.WriteLine("Displaying the list items");
+            DisplayList(list);
+            Console.WriteLine("\nRemove the element");
+            list.Remove("hi");
+            Console.WriteLine("After removal");
+            DisplayList(list);
+            Console.WriteLine("\nElement at 2nd position is {0}", list[2]);
+
+
+        }
+        //Method to display the list
+        public static void DisplayList(List<string> list)
+        {
+            foreach (string l in list)
+            {
+                Console.WriteLine(l);
+            }
+        }
+    }
+}
+
---
+++ Collections/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Collections!");
+            Console.WriteLine("1.List");
+            Console.WriteLine("Enter you choice");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    ListDemo.List();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
